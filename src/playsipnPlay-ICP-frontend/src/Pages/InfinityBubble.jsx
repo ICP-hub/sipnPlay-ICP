2d11481e1523dc089830dcd66a6a4ea4b2483b55
tusharqb <tushar.jain@quadbtech.com>
2024-12-04 11:30:45 +0000

    infinity bubble

--- src/playsipnPlay-ICP-frontend/src/Pages/InfinityBubble.jsx
+++ src/playsipnPlay-ICP-frontend/src/Pages/InfinityBubble.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useAuth } from "../utils/useAuthClient";
 
 const InfinityBubble = () => {
+  const { isAuthenticated } = useAuth();
+  const userData = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!userData.id || !userData.email) {
+      toast.error("You are not logged in! ");
+      navigate("/");
+    } else if (!isAuthenticated) {
+      navigate("/");
+      toast.error("You are not logged in! ");
+    } else {
+      // getDetails();
+    }
+  }, []);
+
   return (
     <iframe
       title="Infinity Bubble"
