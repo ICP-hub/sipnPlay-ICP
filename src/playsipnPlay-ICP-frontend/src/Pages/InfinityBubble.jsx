fa0821154bf8bc82f62ddfd453c869c65a1f663e
somiya <somiya.behra@quadbtech.com>
2024-12-04 17:13:58 +0530

    Initialized Infinity Bubble Files and Authenticated Check

--- src/playsipnPlay-ICP-frontend/src/Pages/InfinityBubble.jsx
+++ src/playsipnPlay-ICP-frontend/src/Pages/InfinityBubble.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../utils/useAuthClient";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import toast from "react-hot-toast";
 
 const InfinityBubble = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, backendActor, principal, ledgerActor } = useAuth();
+  const [score, setScore] = useState(0);
+  const navigate = useNavigate();
   const userData = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const handleScore = (event) => {
+      if (event.data?.type === "save_score") {
+        setScore(event.data.score);
+        console.log("Score from useEffect", score);
+      }
+    };
+    window.addEventListener("message", handleScore);
+    return () => {
+      window.removeEventListener("message", handleScore);
+    };
+  }, []);
 
   useEffect(() => {
     if (!userData.id || !userData.email) {
