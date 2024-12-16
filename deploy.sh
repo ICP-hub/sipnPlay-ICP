9b1d1881361407f6b49f5f62c0b4679c91111e30
somiya <somiya.behra@quadbtech.com>
2024-12-16 16:27:06 +0530

    Admin Leaderboard Changes

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,5 @@ cargo build --release --target wasm32-unknown-unknown --package sipnplay_icp_bac
 candid-extractor target/wasm32-unknown-unknown/release/sipnplay_icp_backend.wasm > src/sipnplay_icp_backend/sipnplay_icp_backend.did
 
 dfx deploy sipnplay_icp_backend
+
+dfx deploy index_canister --argument '(opt variant { Init = record { ledger_id = principal "bkyz2-fmaaa-aaaaa-qaaaq-cai"; retrieve_blocks_from_ledger_interval_seconds = opt 10; } })'
